79e6ac523323d3a3ad835895325b9c7aef4bb9e0
Reena <reena.sanjay.sharma@gmail.com>
2018-03-24 21:51:00 +0000

    created db,installed pg,added knexfile,run migrations

---
+++ knexfile.js
@@ -0,0 +1,39 @@
+// Update with your config settings.
+
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/rating_test',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+  development: {
+    client: 'pg',
+    connection: {
+      'user': 'reena',
+      'database': 'rating_dev',
+      'password': process.env.PG_PASSWORD
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production'
+    }
+  }
+};
+
