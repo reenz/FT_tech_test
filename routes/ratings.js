bda7c5b01a7e3262c4ded5ff40aff41e3b965100
Reena <reena.sanjay.sharma@gmail.com>
2018-03-25 02:42:52 +0100

    replaced var with const, installed eslint

--- routes/ratings.js
+++ routes/ratings.js
@@ -5,14 +5,9 @@ const knex = require("../db/server/knex.js");
 /* GET rating listing. */
 router.post('/', (req, res, next) => {
   const score = req.body["score"];
-  knex("rating").insert({rating: score}).then((out) => {
-    const msg = "Successfully inserted record for score " + score;
-    console.log(msg);
+  knex("rating").insert({rating: score}).then(() => {
+     res.render("ratings");
   });
-  res.render("ratings", {"msg": msg});
-});
-
-router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
